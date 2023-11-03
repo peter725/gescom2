67551f79f2b3c6461a1f39429ecc168fbcc9149d
WosbaldoRamos <wramos@serikat.es>
2023-11-03 11:36:15 +0100

    EndPoint NationalAuthority

---
+++ rest/src/main/java/es/dgc/gesco/controller/NationalAuthorityController.java
@@ -0,0 +1,52 @@
+package es.dgc.gesco.controller;
+
+import es.dgc.gesco.facade.NationalAuthorityFacade;
+
+import es.dgc.gesco.model.modules.nationalAuthority.db.entity.NationalAuthority;
+import es.dgc.gesco.util.Url;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+
+@Log4j2
+@RestController
+@RequestMapping(Url.API+Url.NATIONAL_AUTHORITY)
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = {"Authorization", "Content-Type"}, maxAge = 3600)
+public class NationalAuthorityController{
+
+    @Autowired
+    private NationalAuthorityFacade nationalAuthorityFacade;
+
+
+    @PostMapping(Url.NATIONAL_AUTHORITY)
+    public ResponseEntity<Void> findAll() {
+
+        List<NationalAuthority> nationalAuthorities;
+
+        try {
+
+            nationalAuthorities = nationalAuthorityFacade.findAll();
+
+        } catch (DataIntegrityViolationException ex) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            log.error(e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+}
--- rest/src/main/java/es/dgc/gesco/util/Url.java
+++ rest/src/main/java/es/dgc/gesco/util/Url.java
@@ -14,5 +14,7 @@ public class Url {
     public static final String DELETE = "/delete";
     public static final String UPDATE = "/update";
 
+    public static final String NATIONAL_AUTHORITY ="/nationalAuthority";
+
 
 }

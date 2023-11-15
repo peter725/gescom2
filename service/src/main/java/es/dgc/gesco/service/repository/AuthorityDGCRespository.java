143fe3d2919989a0977d1010e257da6eda9b8291
jprojasext <jprojas.ext@serikat.es>
2023-11-15 13:34:18 -0500

    Delete AuthorityDGCRespository.java

--- service/src/main/java/es/dgc/gesco/service/repository/AuthorityDGCRespository.java
+++
@@ -1,8 +0,0 @@
-package es.dgc.gesco.service.repository;
-
-import es.dgc.gesco.model.modules.authorityDGC.db.entity.AuthorityDGC;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorityDGCRespository extends BaseRepository<AuthorityDGC, Long>{
-}

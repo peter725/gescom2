af977a061bdf28a54adb1a7332b1cdd6e6e11e32
WosbaldoRamos <wramos@serikat.es>
2023-12-21 16:57:02 +0100

    Correccion de Errores Especialistas

--- service/src/main/java/es/dgc/gesco/service/service/SpecialistService.java
+++ service/src/main/java/es/dgc/gesco/service/service/SpecialistService.java
@@ -1,19 +1,24 @@
 package es.dgc.gesco.service.service;
 
+import es.dgc.gesco.model.modules.specialist.db.entity.Specialist;
+import es.dgc.gesco.service.repository.SpecialistRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SpecialistService {
 
-//    @Autowired
-//    private final SpecialistRepository specialistRepository;
-//
-//    public Page<Specialist> getAllByPage(Pageable pageable) {
-//        Page<Specialist> specialistPage = specialistRepository.findAll(pageable);
-//        return specialistPage;
-//    }
+    @Autowired
+    private final SpecialistRepository specialistRepository;
+
+    public Page<Specialist> getAllByPage(Pageable pageable) {
+        Page<Specialist> specialistPage = specialistRepository.findAll(pageable);
+        return specialistPage;
+    }
 
 
 

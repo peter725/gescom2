b243a06c6be5304ca4119b6183488f8fa9d4f576
jprojasext <jprojas.ext@serikat.es>
2024-02-19 07:38:11 -0500

    commit

--- ge-rest-app/src/main/java/es/consumo/gescom/modules/productServices/service/ProductServiceService.java
+++ ge-rest-app/src/main/java/es/consumo/gescom/modules/productServices/service/ProductServiceService.java
@@ -3,11 +3,18 @@ package es.consumo.gescom.modules.productServices.service;
 import es.consumo.gescom.commons.dto.wrapper.CriteriaWrapper;
 import es.consumo.gescom.commons.service.CrudService;
 import es.consumo.gescom.modules.productServices.model.criteria.ProductServiceCriteria;
+import es.consumo.gescom.modules.productServices.model.dto.ProductServiceDTO;
 import es.consumo.gescom.modules.productServices.model.entity.ProductServiceEntity;
 import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface ProductServiceService extends CrudService<ProductServiceEntity, Long>{
 
     Page<ProductServiceEntity.SimpleProjection> findAllProductServiceById(CriteriaWrapper<ProductServiceCriteria> wrapper, Long id);
-    
+
+
+    ProductServiceEntity findByCode(String code);
+
+    ProductServiceEntity findProductServiceById(Long id);
 }

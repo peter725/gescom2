8c6c34c232a78b327275288d18574eca75140930
jprojasext <jprojas.ext@serikat.es>
2024-08-02 08:38:14 -0500

    Update app.ts

--- web/src/app/config/app.ts
+++ web/src/app/config/app.ts
@@ -2,7 +2,7 @@ import { Version } from '@angular/core';
 import { environment } from '@env';
 
 const getVersion = () => {
-  let version = '0.0.2';
+  let version = '0.0.3';
   if (environment.name) {
     version += '-' + environment.name;
   }
